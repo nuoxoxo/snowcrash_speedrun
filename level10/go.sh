85e25c27951a90499b78e0be550a755fde0a6899
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2025-01-04 12:00:56 +0100

    Update go.sh

--- level10/go.sh
+++ level10/go.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
-#timeout 2s bash -c '
-while true; do
-  /home/user/level10/level10 /tmp/tmp 127.0.0.1 >/dev/null
+IP=$(ifconfig | grep 'inet ' | awk 'NR==1 {print $2}' | cut -d ":" -f2)
+while true; do  
+  #/home/user/level10/level10 /tmp/tmp ${IP} >/dev/null
+  /home/user/level10/level10 /tmp/tmp $(hostname -I | awk '{print $1}') >/dev/null
 done
-#'
